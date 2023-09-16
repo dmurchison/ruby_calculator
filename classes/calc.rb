9a2cc57040e14e9d1d8b517980a0afe08c364afa
dmurchison <duncancmurchison@gmail.com>
2023-09-16 02:43:12 -0400

    ♻️ Refactore Calculator class

--- classes/calc.rb
+++ classes/calc.rb
@@ -1,20 +1,20 @@
+# frozen_string_literal: true
 
-class Calculator
-  
+class Calculator # rubocop:disable Style/Documentation
+  attr_accessor :result # This is a shortcut to create a getter and setter method for the @result instance variable
+
   def evaluate(expression)
-    @res = expression.to_i + expression.reverse.to_i
+    a = expression.split(' ') # ['2', '+', '3'] # split the string into an array
+    b = a.map(&:to_i) # { |x| x.to_i } [2, 0, 3] # turn the strings into integers and non numbers into 0
+    @result = b.inject(&:+) # { |sum, x| sum + x } # 5 # add the numbers together
   end
-
-  def result
-    @res
-  end
-
-  
-
 end
 
+c = Calculator.new
+p c.evaluate('2 + 2') # 4
+p c.evaluate('2 + 3') # 5
 
 # SUDO
-# Break the string into operands and operator (regex)
-# Turn the operands into numbers (parse)
-# Do the evaluation
+# Break the string into operands and operator (regex) Didn't use regex, refactored code and would like to here your thoughts on this approach
+# Turn the operands into numbers (parse) ✅
+# Do the evaluation ✅
