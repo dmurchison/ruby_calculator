5aa467a3325ef237f1581b7daeeef3a5ae9dc80b
dmurchison <duncancmurchison@gmail.com>
2023-09-19 11:47:47 -0400

    ♻️ refactor eval method

--- classes/calc.rb
+++ classes/calc.rb
@@ -1,20 +1,15 @@
 # frozen_string_literal: true
 
 class Calculator # rubocop:disable Style/Documentation
-  attr_accessor :result # This is a shortcut to create a getter and setter method for the @result instance variable
+  attr_accessor :result # This is a shortcut to creat
 
-  def evaluate_add(expression)
-    a = expression.split(' ') # ['2', '+', '3'] # split the string into an array
-    b = a.map(&:to_i) # { |x| x.to_i } [2, 0, 3] # turn the strings into integers and non numbers into 0
-    @result = b.inject(&:+) # { |sum, x| sum + x } # 5 # add the numbers together
+  def evaluate(expression)
+    tokens = expression.split(' ') # ['2', '+', '3'] # split the string into an array
+    first_operand = tokens.first.to_i
+    second_operand = tokens.last.to_i
+    @result = first_operand + second_operand
   end
-
-  def evaluate_sub(expression)
-    a = expression.split(' ') # ['2', '+', '3'] # split the string into an array
-    b = a.map(&:to_i) # { |x| x.to_i } [2, 0, 3] # turn the strings into integers and non numbers into 0
-    @result = b.reduce(&:-) # { |sum, x| sum + x } # 5 # add the numbers together
-  end
-end
+end 
 
 # Break the string into operands and operator (regex) Didn't use regex
 # refactored code and would like to here your thoughts on this approach
