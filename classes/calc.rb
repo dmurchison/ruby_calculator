7b626accd1a2ed485c9227e4b7c8fce5a9ad2e9c
dmurchison <duncancmurchison@gmail.com>
2023-09-21 13:33:04 -0400

    🔥 first attempt at divide spec

--- classes/calc.rb
+++ classes/calc.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
-require('pry-byebug')
+require 'rspec'
+require 'pry-byebug'
 
-class Calculator # rubocop:disable Style/Documentation
-  attr_accessor :result
+# Creating a Calculator class
+class Calculator
+  attr_accessor :result, :operator, :first_operand, :second_operand
 
-  def evaluate(expression) # rubocop:disable Metrics/MethodLength
+  def evaluate(expression) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
     tokens = expression.split(' ') # ['2', '+', '3'] # split the string into an array
-    first_operand = tokens.first.to_i
-    second_operand = tokens.last.to_i
-    operator = tokens[1] # Assuming the operator is always in the middle
+    @first_operand = tokens.first.to_i
+    @second_operand = tokens.last.to_i
+    @operator = tokens[1] # Assuming the operator is always in the middle
     # binding.pry
     case operator # I don't think this is doing anything different than the if statement, but I like it better
     when '+'
@@ -18,16 +20,18 @@ class Calculator # rubocop:disable Style/Documentation
       @result = first_operand - second_operand
     when '*'
       @result = first_operand * second_operand
+    when '/'
+      @result = first_operand / second_operand
     end
     # binding.pry
     @result
   end
 end
 
-puts Calculator.new.evaluate('2 * 3') # 6
-
-# Break the string into operands and operator (regex) Didn't use regex
-# refactored code and would like to here your thoughts on this approach
-
-# test:
-# don't change first three lines of eval method and get subtract methods to pass.
+c = Calculator.new
+puts c.evaluate('2 + 3') # 5
+puts c.evaluate('2 - 3') # -1
+puts c.evaluate('2 * 3') # 6
+puts c.evaluate('12 / 12') # 0
+# puts c.evaluate('2 + 3 - 4') # 1
+# Using RSpec TDD make this calculator work with more than one operation at a time ie "2 + 3 - 4" should return 1.
--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -1,37 +1,49 @@
 # frozen_string_literal: true
 
 require_relative '../classes/calc' # First I need to load the class/file I want to test
+require 'rspec'
 
-RSpec.describe Calculator do # I'm describing the Calculator class (from calc.rb)
+RSpec.describe Calculator do # rubocop:disable Metrics/BlockLength
   let(:calc) { Calculator.new } # This is a helper method that will create a new instance of the Calculator class
 
   it 'exists' do
     expect(calc).to be_a(Calculator)
   end
 
-  it 'can add the same two numbers' do
+  it 'can add the same number' do
     calc.evaluate('2 + 2')
     expect(calc.result).to eq(4)
   end
 
   it 'can add two different numbers' do
-    # This is a different test yes very similar to the one above
+    # This is a different test yet very similar to the one above, I don't understand 
     calc.evaluate('4 + 5')
     expect(calc.result).to eq(9)
   end
 
-  it 'can subtract two of the same numbers' do
+  it 'can subtract the same number' do
     calc.evaluate('5 - 5')
     expect(calc.result).to eq(0)
   end
 
-  it 'can subtract two numbers' do
+  it 'can subtract two different numbers' do
     calc.evaluate('5 - 2')
     expect(calc.result).to eq(3) # This test will fail
   end
 
-  it 'can multiply two numbers' do
-    calc.evaluate('6 * 3')
-    expect(calc.result).to eq(18)
+  it 'can multiply the same number' do
+    calc.evaluate('6 * 6')
+    expect(calc.result).to eq(36)
+  end
+
+  it 'can multiply two different numbers' do
+    calc.evaluate('6 * 7')
+    expect(calc.result).to eq(42)
+  end
+
+  it 'can divide the same number' do
+    if calc.operator == '/' && calc.first_operand == calc.second_operand
+      raise Error, 'This will always be 1'
+    end
   end
 end
