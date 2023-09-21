5ce535ee51b064bdcd9c8f7a57319d09df2be3a2
dmurchison <duncancmurchison@gmail.com>
2023-09-21 18:05:09 -0400

    ✅ Add more passing specs

--- classes/calc.rb
+++ classes/calc.rb
@@ -1,37 +1,55 @@
 # frozen_string_literal: true
 
-require 'rspec'
-require 'pry-byebug'
+require "rspec"
+require "pry-byebug"
 
 # Creating a Calculator class
 class Calculator
-  attr_accessor :result, :operator, :first_operand, :second_operand
+  attr_accessor :result
 
-  def evaluate(expression) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
-    tokens = expression.split(' ') # ['2', '+', '3'] # split the string into an array
-    @first_operand = tokens.first.to_i
-    @second_operand = tokens.last.to_i
-    @operator = tokens[1] # Assuming the operator is always in the middle
+  def evaluate(expression)
+    tokens = expression.split(" ")
+    first_operand = tokens.first.to_i
+    second_operand = tokens.last.to_i
+    operator = tokens[1] # This is going to have to be refactored.
+
     # binding.pry
+
     case operator # I don't think this is doing anything different than the if statement, but I like it better
-    when '+'
+    when "+"
       @result = first_operand + second_operand
-    when '-'
+    when "-"
       @result = first_operand - second_operand
-    when '*'
+    when "*"
       @result = first_operand * second_operand
-    when '/'
-      @result = first_operand / second_operand
+    when "/"
+
+      begin
+        @result = first_operand / second_operand
+      rescue ZeroDivisionError
+        @result = "You can't divide by 0!"
+      end
+
+      if first_operand < second_operand
+        @result = "In division, if the second operand is larger, the answer will always be 0!"
+      elsif first_operand == second_operand
+        @result = "In division, if the two operands are the same, the answer will always be 1!"
+      end
+
     end
-    # binding.pry
     @result
   end
+
 end
 
 c = Calculator.new
-puts c.evaluate('2 + 3') # 5
-puts c.evaluate('2 - 3') # -1
-puts c.evaluate('2 * 3') # 6
-puts c.evaluate('12 / 12') # 0
-# puts c.evaluate('2 + 3 - 4') # 1
+# p c.evaluate("2 + 3") # 5
+# p c.evaluate("2 - 3") # -1
+# p c.evaluate("2 * 3") # 6
+p c.evaluate("12 / 0") # ZeroDivisionError
+p c.evaluate("2 / 12") # Always 0
+p c.evaluate("15 / 15") # Always 1
+# p c.evaluate("2 + 3 - 4") # 1
+
 # Using RSpec TDD make this calculator work with more than one operation at a time ie "2 + 3 - 4" should return 1.
+# Create assertion about evaluation and execution
--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -1,49 +1,47 @@
 # frozen_string_literal: true
 
-require_relative '../classes/calc' # First I need to load the class/file I want to test
-require 'rspec'
+require_relative "../classes/calc" # First I need to load the class/file I want to test
+require "rspec"
 
-RSpec.describe Calculator do # rubocop:disable Metrics/BlockLength
+RSpec.describe Calculator do
   let(:calc) { Calculator.new } # This is a helper method that will create a new instance of the Calculator class
 
-  it 'exists' do
+  it "exists" do
     expect(calc).to be_a(Calculator)
   end
 
-  it 'can add the same number' do
-    calc.evaluate('2 + 2')
-    expect(calc.result).to eq(4)
-  end
-
-  it 'can add two different numbers' do
-    # This is a different test yet very similar to the one above, I don't understand 
-    calc.evaluate('4 + 5')
+  it "can add two numbers" do
+    calc.evaluate("4 + 5")
     expect(calc.result).to eq(9)
   end
 
-  it 'can subtract the same number' do
-    calc.evaluate('5 - 5')
-    expect(calc.result).to eq(0)
+  it "can subtract two numbers" do
+    calc.evaluate("5 - 2")
+    expect(calc.result).to eq(3)
   end
 
-  it 'can subtract two different numbers' do
-    calc.evaluate('5 - 2')
-    expect(calc.result).to eq(3) # This test will fail
-  end
-
-  it 'can multiply the same number' do
-    calc.evaluate('6 * 6')
+  it "can multiply two numbers" do
+    calc.evaluate("6 * 6")
     expect(calc.result).to eq(36)
   end
 
-  it 'can multiply two different numbers' do
-    calc.evaluate('6 * 7')
-    expect(calc.result).to eq(42)
+  it "returns a string explaining why this will always return 0" do
+    calc.evaluate("10 / 15")
+    expect(calc.result).to be_a(String)
   end
 
-  it 'can divide the same number' do
-    if calc.operator == '/' && calc.first_operand == calc.second_operand
-      raise Error, 'This will always be 1'
-    end
+  it "returns a string explaining why this will always return 1" do
+    calc.evaluate("10 / 10")
+    expect(calc.result).to be_a(String)
   end
+
+  it "can divide by 0 without raising ZeroDivisionError and an explanation" do
+    expect { calc.evaluate("5 / 0") }.to_not raise_error
+  end
+
+  xit "can evaluate multiple operations" do
+    calc.evaluate("2 + 3 - 4")
+    expect(calc.result).to eq(1)
+  end
+
 end
