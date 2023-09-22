de2686c7e79264b75b55274b5d24ae5c9e6fda7d
dmurchison <duncancmurchison@gmail.com>
2023-09-22 18:59:49 -0400

    🚀

--- classes/calc.rb
+++ classes/calc.rb
@@ -8,39 +8,37 @@ class Calculator
   attr_accessor :result
 
   def evaluate(expression)
+
     tokens = expression.split(" ")
     operator = nil
-    first_operand = nil
     second_operand = nil
+
     tokens.each do |token|
-      if !first_operand
-        first_operand = token.to_i
+      if @result.nil?
+        @result = token.to_i
         next
       end
-      if !operator
+      if operator.nil?
         operator = token
         next
       end
       second_operand = token.to_i
       case operator # I don't think this is doing anything different than the if statement, but I like it better
       when "+"
-        @result = first_operand + second_operand
+        @result += second_operand
       when "-"
-        @result = first_operand - second_operand
+        @result -= second_operand
       when "*"
-        @result = first_operand * second_operand
+        @result *= second_operand
       when "/"
         begin
-          @result = first_operand / second_operand
+          @result /= second_operand
         rescue ZeroDivisionError
           @result = "You can't divide by 0!"
         end
-        if first_operand < second_operand
-          @result = "In division, if the second operand is larger, the answer will always be 0!"
-        elsif first_operand == second_operand
-          @result = "In division, if the two operands are the same, the answer will always be 1!"
-        end
       end
+      operator = nil
+      second_operand = nil
     end
   end
 
@@ -51,9 +49,13 @@ end
 # p c.evaluate("2 - 3") # -1
 # p c.evaluate("2 * 3") # 6
 # p c.evaluate("12 / 0") # ZeroDivisionError
-# p c.evaluate("2 / 12") # Always 0
 # p c.evaluate("15 / 15") # Always 1
 # p c.evaluate("2 + 3 - 4") # 1
 
 # Using RSpec TDD make this calculator work with more than one operation at a time ie "2 + 3 - 4" should return 1.
 # Create assertion about evaluation and execution
+
+# Defect: Mistake in the code ()
+# Error: Is when we get
+
+# Write a test that can handle string inputs and the spacing between operators and operands
--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -6,42 +6,33 @@ require "rspec"
 RSpec.describe Calculator do
   let(:calc) { Calculator.new } # This is a helper method that will create a new instance of the Calculator class
 
-  it "exists" do
-    expect(calc).to be_a(Calculator)
-  end
+    it "exists" do
+      expect(calc).to respond_to?(:evaluate)
+    end
 
-  it "can add two numbers" do
-    calc.evaluate("4 + 5")
-    expect(calc.result).to eq(9)
-  end
+    it "can add two numbers" do
+      calc.evaluate("4 + 5")
+      expect(calc.result).to eq(9)
+    end
 
-  it "can subtract two numbers" do
-    calc.evaluate("5 - 2")
-    expect(calc.result).to eq(3)
-  end
+    it "can subtract two numbers" do
+      calc.evaluate("5 - 2")
+      expect(calc.result).to eq(3)
+    end
 
-  it "can multiply two numbers" do
-    calc.evaluate("6 * 6")
-    expect(calc.result).to eq(36)
-  end
+    it "can multiply two numbers" do
+      calc.evaluate("6 * 6")
+      expect(calc.result).to eq(36)
+    end
 
-  it "returns a string explaining why this will always return 0" do
-    calc.evaluate("10 / 15")
-    expect(calc.result).to be_a(String)
-  end
+    it "can divide by 0 without raising ZeroDivisionError and an explanation" do
+      expect { calc.evaluate("5 / 0") }.to_not raise_error
+      expect(calc.result).to be_a(String)
+    end
 
-  it "returns a string explaining why this will always return 1" do
-    calc.evaluate("10 / 10")
-    expect(calc.result).to be_a(String)
-  end
-
-  it "can divide by 0 without raising ZeroDivisionError and an explanation" do
-    expect { calc.evaluate("5 / 0") }.to_not raise_error
-  end
-
-  xit "can evaluate multiple operations" do
-    calc.evaluate("2 + 3 - 4")
-    expect(calc.result).to eq(1)
-  end
+    it "can evaluate multiple operations" do
+      calc.evaluate("2 + 3 - 4")
+      expect(calc.result).to eq(1)
+    end
 
 end
